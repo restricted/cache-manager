231072c58692f2ceb628bced54c7901252abc1c8
Dementyev AlexeyHome <adementyev@zaoblako.ru>
2019-02-28 21:38:37 +0300

    [FEAT] - before / after tests hook

--- index.test.ts
+++ index.test.ts
@@ -16,39 +16,40 @@ before(async () => {
     return await cache.connect();
 });
 
-after(async () => {
+after(() => {
     if (cache) {
-        return await cache.disconnect();
+        cache.disconnect();
     }
+    process.exit();
 });
 
-describe('RedisDataCache test suite', () => {
+describe('RedisDataCache test suite', async () => {
 
-    it('should create default Redis connection', () => {
-        expect(['connect', 'connecting', 'ready'].indexOf(cache.status())).is.greaterThan(-1);
+    await it('should create default Redis connection', async () => {
+        await expect(['connect', 'connecting', 'ready'].indexOf(cache.status())).is.greaterThan(-1);
     });
 
-    it('should create Redis connection by url', () => {
-        cache = new RedisDataCache({url: 'redis://127.0.0.1:6379'});
-        expect(cache.status()).is.equal('connecting');
+    await it('should create Redis connection by url', async () => {
+        cache = await new RedisDataCache({url: 'redis://127.0.0.1:6379'});
+        await expect(cache.status()).is.equal('connecting');
     });
 
     const example = new ExampleTest();
     example.id = 1;
     example.name = 'Hello';
 
-    it('should create object in Redis', () => {
-        cache.create(example).then((ok) => {
+    await it('should create object in Redis', async () => {
+        await cache.create(example).then((ok) => {
             expect(ok).equal('OK');
         }).catch(e => {
             console.error(e);
         });
     });
 
-    it('should exists created object in Redis', () => {
-        cache.create(example).then((ok) => {
+    await it('should exists created object in Redis', async () => {
+        await cache.create(example).then(async (ok) => {
             expect(ok).equal('OK');
-            cache.findById(ExampleTest, example.id).then(e => {
+            await cache.findById(ExampleTest, example.id).then(e => {
                 expect(e).to.deep.equal(example);
             }).catch(e => {
                 console.error(e);
@@ -58,10 +59,10 @@ describe('RedisDataCache test suite', () => {
         });
     });
 
-    it('should exists created object in Redis by findByIds', () => {
-        cache.create(example).then((ok) => {
+    await it('should exists created object in Redis by findByIds', async () => {
+        await cache.create(example).then(async (ok) => {
             expect(ok).equal('OK');
-            cache.findByIds(ExampleTest, [example.id]).then(e => {
+            await cache.findByIds(ExampleTest, [example.id]).then(e => {
                 expect(e[0]).to.deep.equal(example);
             }).catch(e => {
                 console.error(e);
@@ -70,5 +71,4 @@ describe('RedisDataCache test suite', () => {
             console.error(e);
         });
     });
-})
-;
+});
